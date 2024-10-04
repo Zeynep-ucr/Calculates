ad8dc908952376c52936486d48556c8fca56790c
Zeynep-ucr <zeynepucar129.828@gmail.com>
2024-10-04 13:31:22 +0300

    multiplication function added

--- Calculate.py
+++ Calculate.py
@@ -2,11 +2,13 @@ def addition(a, b):
     return a + b
 def subtraction(a, b):
     return a - b
-
+def multiplication(a, b):
+    return a * b
 def main():
     # Get two numbers from the user
-    number1 = float(input("Enter the first number: 12"))
+    number1 = float(input("Enter the first number: "))
     number2 = float(input("Enter the second number: "))
     print(f"{number1} + {number2} = {addition(number1, number2)}")
+    print(f"{number1} - {number2} = {subtraction(number1, number2)}")
 if __name__ == '__main__':
     main()
