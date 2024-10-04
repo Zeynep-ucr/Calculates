82ffb8f92bb3bb4b6d63d0d62bc5a35ce8c39a63
Zeynep-ucr <zeynepucar129.828@gmail.com>
2024-10-04 13:21:00 +0300

    subtraction function added

--- Calculate.py
+++ Calculate.py
@@ -5,7 +5,7 @@ def subtraction(a, b):
 
 def main():
     # Get two numbers from the user
-    number1 = float(input("Enter the first number: "))
+    number1 = float(input("Enter the first number: 12"))
     number2 = float(input("Enter the second number: "))
     print(f"{number1} + {number2} = {addition(number1, number2)}")
 if __name__ == '__main__':
