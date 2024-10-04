e352eba9579a715a1d5a01a07a8dea612845b9a1
Zeynep-ucr <zeynepucar129.828@gmail.com>
2024-10-04 13:15:04 +0300

    Calculate created saturate addition function added

---
+++ Calculate.py
@@ -0,0 +1,9 @@
+def addition(a, b):
+    return a + b
+def main():
+    # Get two numbers from the user
+    number1 = float(input("Enter the first number: "))
+    number2 = float(input("Enter the second number: "))
+    print(f"{number1} + {number2} = {addition(number1, number2)}")
+if __name__ == '__main__':
+    main()
